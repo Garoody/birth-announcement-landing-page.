dc72ee2fa426a6a964995d46e08373e17926cd9f
Garoody chery <231674192+Garoody@users.noreply.github.com>
2025-10-20 15:31:44 +0200

    feat(ui):I am starting to modify the background images

---
+++ assets/js/main.js
@@ -0,0 +1,57 @@
+/** 
+*
+*Fonction toggle pour activer ou désactiver la fenêtre modale de la newsletter
+
+*Ce code ajoute un gestionnaire  de de clic à tous les éléments ayant la classe "show__newsletter".
+*Lorsque l'un de ces éléments est cliqué, il bascule l'état de visibilité de l'élément ayant
+*la classe "newsletter" en ajoutant ou en supprimant la classe "active".
+*Cela permet de montrer ou de cacher la newsletter, selon son état actuel.
+*
+*/
+/**
+* 
+* const SHOW_NEWSLETTER = document.getElementsByClassName("show__newsletter");
+* 
+*Cette ligne sélectionne tous les éléments de la page HTML qui ont la classe "show__newsletter"
+et les stocke dans une variable appelée SHOW_NEWSLETTER.
+Cela retourne un objet semblable à un tableau, appelé une collection HTML.
+*
+*/
+const SHOW_NEWSLETTER = document.getElementsByClassName("show__newsletter");
+/*  console.log(SHOW_NEWSLETTER);
+/**
+*
+* for (const show of SHOW_NEWSLETTER)
+Cette ligne initialise une boucle for...of qui parcourt chaque élément de la collection SHOW_NEWSLETTER. 
+*
+*/
+for (const show of SHOW_NEWSLETTER) {   
+/**
+*
+*show.addEventListener("click", () => {}
+*Cette ligne ajoute un écouteur d'événement de clic à chaque élément show de la collection.
+*Lorsqu'un élément est cliqué, la fonction fléchée (() => { ... }) est exécutée.
+*
+*/
+show.addEventListener("click", () => { 
+/** 
+*
+*const newsletter = document.querySelector(".newsletter");
+*
+*Cette ligne sélectionne le premier élément de la page HTML ayant la classe
+*"newsletter" et le stoke dans la variable appelée newsletter.
+*La méthode querySelector retourne uniquement le premier élément correspond
+*à la requête spécifiée.
+*
+*/
+const newsletter = document.querySelector(".newsletter");
+/**
+*
+*newsletter.classlist.toggle("active"); -
+*Cette ligne utilise la méthode classlist.toggle pour ajouter ou supprimer la classe 
+*"active" de l'élément newsletter. Si l'élément a déjà la classe "active", elle est supprimée;
+*sinon, elle est ajoutée. Cela permet de basculer l'état de visibilité de l'élément.
+*/
+newsletter.classList.toggle("active");    
+});
+  }
